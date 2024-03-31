f14f44077a188592a955d6f3c1102dc9161dbf1e
Antoine portable <Digit4l@outlook.fr>
2024-03-31 23:32:55 +0530

    comments traduction for the KNN and PCA

--- PCA.py
+++ PCA.py
@@ -2,30 +2,27 @@ import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
 from sklearn.decomposition import PCA
 
-# Étape 1: Chargement des données
-# Remplacer 'data.csv' par le chemin de votre fichier de données
+# Load data
 file_path = 'dataset/merged.csv'
 data = pd.read_csv(file_path)
 data['genres'] = data['genres'].apply(lambda x: x.split('|'))
 
-# Étape 2: Prétraitement des données
-# Transformation des genres en one-hot encoding
+# Transform the genres into one-hot encoding
 mlb = MultiLabelBinarizer()
 genres_encoded = mlb.fit_transform(data['genres'])
 genres_encoded_df = pd.DataFrame(genres_encoded, columns=mlb.classes_)
 
-# Concaténation des caractéristiques numériques avec les genres encodés
+# Concatenation of numerical features with the encoded genres
 features = pd.concat([data[['rating']], genres_encoded_df], axis=1)
 
-# Étape 3: Normalisation des données
+# Normalize the features
 scaler = StandardScaler()
 features_scaled = scaler.fit_transform(features)
 
-# Étape 4: Application de la PCA
-pca = PCA(n_components=2)  # Vous pouvez ajuster le nombre de composantes ici
+# Application for PCA
+pca = PCA(n_components=2)
 principal_components = pca.fit_transform(features_scaled)
 principal_df = pd.DataFrame(data = principal_components,
                             columns = ['Principal Component 1', 'Principal Component 2'])
 
-# Affichage des résultats
 print(principal_df.head())
